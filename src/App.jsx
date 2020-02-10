aa72e27a9031d9b36fdd59c8e6bc4c233a0723f0
boye <boye_1990@aliyun.com>
2020-02-10 21:23:53 +0800

    恢复单个context

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import React, { createContext, useState } from 'react';
 // 通过createContext函数创建context，该函数可以传入一个context的默认值
 const BatteryContext = createContext();
 
-// 创建多个context
-const OnlineContext = createContext();
-
 // 创建一个消费context的组件
 function Leaf() {
   return (
@@ -14,13 +11,7 @@ function Leaf() {
     <BatteryContext.Consumer>
       {/* 使用context */}
       {
-        battery => (
-          <OnlineContext.Consumer>
-            {
-              online => <h1>Battery: { battery } Online: { String(online) }</h1>
-            }
-          </OnlineContext.Consumer>
-        )
+        battery => <h1>Battery: { battery }</h1>
       }
     </BatteryContext.Consumer>
   );
@@ -35,25 +26,16 @@ function Middle() {
 
 function App() {
   const [battery, setBattery] = useState(60);
-  const [online, setOnline] = useState(false);
   return (
     // 创建Context的Provider生产者
     <BatteryContext.Provider value = { battery }>
-      <OnlineContext.Provider value = { online }>
         <button
           type='button'
           onClick={() => setBattery(battery-1)}
         >
           Press
         </button>
-        <button
-          type='button'
-          onClick={() => setOnline(!online)}
-        >
-          Switch
-        </button>
         <Middle/>
-      </OnlineContext.Provider>
     </BatteryContext.Provider>
   );
 }
